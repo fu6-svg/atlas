934579e5816662f28cbc512a7c5fd5bd753829cc
Lichen Fu <fu6@engineering.upenn.edu>
2026-07-06 15:58:45 -0700

    Sprint 0:Add Robot Status Monitor

---
+++ robot_status.py
@@ -0,0 +1,9 @@
+robot_name = "Atlas"
+speed = 12
+battery = 98
+mode = "Auto"
+
+print("Robot Name:", robot_name)
+print("Speed:", speed, "km/h")
+print("Battery:", battery, "%")
+print("Mode:", mode)
